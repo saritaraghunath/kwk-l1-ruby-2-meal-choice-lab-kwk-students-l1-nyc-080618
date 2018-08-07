72708372a256cea9e0955c9318132bf520768c94
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-07 14:58:16 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -21,3 +21,10 @@ puts snacks("Swedish fish")
 # Call your methods without any arguments here
 puts snacks
 ## In our snacks method, the default choice is "Cheetos" Calling snacks like this without specifying a choice will return "Any time, is the right time for Cheetos!"
+
+
+
+
+def breakfast(food)
+food = get.chomp
+end
